c5d3fba0d86d669868acd5feb6e1ce3ca9939e60
jailsonh3 <jailsonh3@github.com>
2023-02-26 01:36:31 -0300

    finishind project ignite shop

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['files.stripe.com'],
+  },
+
   reactStrictMode: true,
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Image from 'next/image';
+
+import { globalStyles } from '@/styles/global'
+globalStyles();
+
+import { Container, Header } from '@/styles/pages/app';
+
+import logoImg from '../assets/logo.svg'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
